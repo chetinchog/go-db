e8ae5689f9fc654ab4ac466bf25adb929ac8bb74
iCTG <chetinchog@yahoo.com>
2020-08-08 21:21:05 -0300

    Find

--- godbgorm/main.go
+++ godbgorm/main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chetinchog/go-db/godbgorm/model"
 	"github.com/chetinchog/go-db/godbgorm/storage"
 )
 
 func main() {
-	driver := storage.Postgres
+	driver := storage.MySQL
 	storage.New(driver)
 
-	storage.DB().AutoMigrate(
-		&model.Product{},
-		&model.InvoiceHeader{},
-		&model.InvoiceItem{},
-	)
+	// // Migration
+	// storage.DB().AutoMigrate(
+	// 	&model.Product{},
+	// 	&model.InvoiceHeader{},
+	// 	&model.InvoiceItem{},
+	// )
+
+	// // Create
+	// product1 := model.Product{
+	// 	Name:  "Curso de GO",
+	// 	Price: 120,
+	// }
+	// obs := "Testing con GO"
+	// product2 := model.Product{
+	// 	Name:         "Curso de Testing",
+	// 	Observations: &obs,
+	// 	Price:        150,
+	// }
+	// product3 := model.Product{
+	// 	Name:  "Curso de Python",
+	// 	Price: 200,
+	// }
+	// storage.DB().Create(&product1)
+	// storage.DB().Create(&product2)
+	// storage.DB().Create(&product3)
+
+	// // Find
+	// listProduct := make([]model.Product, 0)
+	// storage.DB().Find(&listProduct)
+	// for _, product := range listProduct {
+	// 	fmt.Printf("%d - %s\n", product.ID, product.Name)
+	// }
+
+	// Find One
+	product := model.Product{}
+	storage.DB().First(&product, 3)
+	fmt.Printf("%d - %s\n", product.ID, product.Name)
 }
